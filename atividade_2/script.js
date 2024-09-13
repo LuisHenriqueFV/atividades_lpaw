2f2bb8bbe1ddd7345e1efbe228509ad3f031d6c1
Luis Henrique Fonseca Victoria <luishenriquefonsecavictoria.dev@gmail.com>
2024-09-12 21:14:38 -0300

    att atividade

--- atividade_2/script.js
+++ atividade_2/script.js
@@ -2,26 +2,27 @@ window.onload = function() {
     const canvas = document.getElementById('gameCanvas');
     const ctx = canvas.getContext('2d');
 
-    // Ajusta o tamanho do canvas para ocupar toda a área disponível
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight - 100;
 
-    // Exibe a imagem logo.png no canvas ao carregar a página
     const backgroundImage = new Image();
-    backgroundImage.src = '../atividade_2/logo.png'
+    backgroundImage.src = '../atividade_2/logo.png';
 
-    // Quando a imagem carregar, desenha-a no canvas
     backgroundImage.onload = function() {
         ctx.drawImage(backgroundImage, 0, 0, canvas.width, canvas.height);
     };
 
-    // Evento de clique para o botão "Iniciar Jogo"
     document.getElementById('start-btn').addEventListener('click', function() {
-        // Limpa o canvas e substitui o conteúdo
         ctx.clearRect(0, 0, canvas.width, canvas.height);
+        initGame(); 
+    });
 
-        // Exibe o novo conteúdo no canvas (aqui você pode inserir a lógica do jogo)
-        initGame(); // Sua lógica de jogo vai aqui
+    anime({
+        targets: '#start-btn',
+        scale: [0.5, 1],
+        duration: 1000,
+        easing: 'easeOutElastic',
+        delay: 500
     });
 };
 
@@ -29,9 +30,6 @@ function initGame() {
     const canvas = document.getElementById('gameCanvas');
     const ctx = canvas.getContext('2d');
     
-    // Desenhe algo novo no canvas quando o jogo começar (substitua isso com sua lógica)
-    // ctx.fillStyle = "#ff6b6b";
-    // ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-    // Outras lógicas do jogo podem ser adicionadas aqui
+    ctx.fillStyle = "#ff6b6b";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
 }
